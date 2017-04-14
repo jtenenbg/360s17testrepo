2962fa78ee08843ff0e241753cea45a16a151e11
Zachary Chandler <Zachary.A.Chandler96@gmail.com>
2017-04-14 11:22:47 -0700

    group 4 rocks!

--- whorocks.java
+++ whorocks.java
@@ -1,12 +1,1 @@
-// Group 2 rocks!!
-
-/* do we have to worry about other stuff in this file?
-   like can we just add extra stuff for fun.
-   is this really fun?
-   decreasing line space
-   and then increasing line space
-   can look kinda cool in some text editors
-   but it doesn't look so good sometimes with line wrap
-
-   */
-
+// Group 4 rocks!!
